d0c890ffe5ba16a4a6e443032ccac3e002c300a5
Andreas Gies <andreas@wayofquality.de>
2024-07-25 22:14:11 +0200

    Add bufferline plugin

--- lua/atooni/lazy.lua
+++ lua/atooni/lazy.lua
@@ -18,6 +18,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  change_detection = {
+    notify = false,
+  },
   spec = {
     -- import your plugins
     { import = "atooni.plugins" },
