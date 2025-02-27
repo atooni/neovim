167b6fe7890c72e6041403e3165b4b9fee1bd93c
Andreas Gies <andreas@wayofquality.de>
2025-02-27 13:46:44 +0100

    Basic Settings

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.opt.nu = true 
+vim.opt.rnu = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
