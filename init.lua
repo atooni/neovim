be2d681d9505172f84387a67420787ad8de77164
Andreas Gies <andreas@wayofquality.de>
2025-02-27 14:48:48 +0100

    Add lazy config for colorschemes

--- init.lua
+++ init.lua
@@ -6,8 +6,43 @@ vim.opt.splitright = true
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
+-- Use the tabstop value for indent and dedent 
+vim.opt.shiftwidth = 0 
 
+-- Synchronizesthe NeoVim Clipboard with the System ClipBoard
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.scrolloff = 999
 
+vim.opt.virtualedit = "block"
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+
+-- Configure the Lazy Package Manager 
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "iruzo/matrix-nvim",
+  "rebelot/kanagawa.nvim"
+})
+
+vim.cmd.colorscheme("matrix")
