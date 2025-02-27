de01742967467102d180509677bc78bc5c04f39f
Andreas Gies <andreas@wayofquality.de>
2025-02-27 18:28:14 +0100

    Splitting config files

---
+++ lua/options.lua
@@ -0,0 +1,22 @@
+vim.opt.nu = true 
+vim.opt.rnu = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+-- Use the tabstop value for indent and dedent 
+vim.opt.shiftwidth = 0 
+
+-- Synchronizesthe NeoVim Clipboard with the System ClipBoard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
