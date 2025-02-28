f43b146dff95a9d44b842d1fd167a78f5143a367
Andreas Gies <Andreas.Gies.Extern@de.ebmpapst.com>
2025-02-28 10:29:39 +0100

    Add preliminary clangd config

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 require("options")
 require("plugins")
+
+-- TODO: Handle properly 
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function() 
+    vim.lsp.start({
+      name = 'clangd',
+      cmd = {'/usr/bin/clangd'},
+      root_dir = vim.fn.getcwd()
+    })
+  end
+})
+
