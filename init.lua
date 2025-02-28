8ab57090000a45a16401272fdc0e454fc96335a4
Andreas Gies <andreas@wayofquality.de>
2025-02-28 11:15:04 +0100

    Remove obsolete code so clangd only handles c files

--- init.lua
+++ init.lua
@@ -1,14 +1,3 @@
 require("options")
 require("plugins")
 
--- TODO: Handle properly 
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function() 
-    vim.lsp.start({
-      name = 'clangd',
-      cmd = {'/usr/bin/clangd'},
-      root_dir = vim.fn.getcwd()
-    })
-  end
-})
-
