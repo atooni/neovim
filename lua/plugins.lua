b848fbfb4f1033a9d645f678b5c58149f138a840
Andreas Gies <Andreas.Gies.Extern@de.ebmpapst.com>
2025-02-28 12:37:28 +0100

    Ensure Language server setup is called

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,9 +47,14 @@ require("lazy").setup({
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "mason.nvim" },
     config = function()
-      local masonLsp = require("mason-lspconfig")
-      masonLsp.setup({
+      local mason_lsp = require("mason-lspconfig")
+      mason_lsp.setup({
         ensure_installed = { "clangd" }
+      })
+      mason_lsp.setup_handlers({
+        function(server_name) 
+          require("lspconfig")[server_name].setup({})
+        end
       })
     end
   },
