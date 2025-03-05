a8d7dd32a2ac0f5790932cc2cba5d8a3cb9177ad
Andreas Gies <andreas@wayofquality.de>
2025-03-05 16:33:56 +0100

    set up formatting on save for typescript files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,130 +1,142 @@
 local map = vim.keymap.set
 
 local function contains(arr, x)
-  for _, v in ipairs(arr) do
-    if v == x then
-      return true
-    end
-  end
-  return false
+	for _, v in ipairs(arr) do
+		if v == x then
+			return true
+		end
+	end
+	return false
 end
 
-return(
-{
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      local mason = require("mason")
-      mason.setup()
-    end
-  },
-  {
-    -- bridge mason and nvim-lspconfig
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "mason.nvim" },
-    config = function()
-      local mason_lsp = require("mason-lspconfig")
-      mason_lsp.setup({
-        ensure_installed = { "clangd", "lua_ls", "ts_ls", "eslint" }
-      })
-      mason_lsp.setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-        -- language specific setups if required 
-        -- See language server docs for config options 
-        ["ts_ls"] = function()
-          require("lspconfig")["ts_ls"].setup({
-            on_attach = function(client, _)
-              client.server_capabilities.documentFormattingProvider = false
-              client.server_capabilities.documentRangeFormattingProvider = false
-            end,  
-            handlers = {
-              ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			local mason = require("mason")
+			mason.setup()
+		end,
+	},
+	{
+		-- bridge mason and nvim-lspconfig
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "mason.nvim" },
+		config = function()
+			local mason_lsp = require("mason-lspconfig")
+			mason_lsp.setup({
+				ensure_installed = { "clangd", "lua_ls", "ts_ls", "eslint" },
+			})
+			mason_lsp.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				-- language specific setups if required
+				-- See language server docs for config options
+				["ts_ls"] = function()
+					require("lspconfig")["ts_ls"].setup({
+						on_attach = function(client, _)
+							client.server_capabilities.documentFormattingProvider = false
+							client.server_capabilities.documentRangeFormattingProvider = false
+						end,
+						handlers = {
+							["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+								print("TS_LS DIAGNOSTICS HANDLER")
+								print("Number of diagnostics: " .. #result.diagnostics)
 
-                print("TS_LS DIAGNOSTICS HANDLER")
-                print("Number of diagnostics: " .. #result.diagnostics)
+								-- Log each diagnostic
+								for i, diag in ipairs(result.diagnostics) do
+									vim.print(diag)
+								end
 
-                -- Log each diagnostic
-                for i, diag in ipairs(result.diagnostics) do
-                  vim.print(diag)
-                end
+								-- Filter out ALL unused variable warnings
+								local ignored_codes = { 6133, 6138 }
+								local filtered_diagnostics = {}
+								for _, diagnostic in ipairs(result.diagnostics) do
+									if not contains(ignored_codes, diagnostic.code) then
+										table.insert(filtered_diagnostics, diagnostic)
+									end
+								end
 
-                -- Filter out ALL unused variable warnings
-                local ignored_codes = { 6133, 6138 }
-                local filtered_diagnostics = {}
-                for _, diagnostic in ipairs(result.diagnostics) do
-                  if not contains(ignored_codes, diagnostic.code) then
-                    table.insert(filtered_diagnostics, diagnostic)
-                  end
-                end
+								print("After filtering: " .. #filtered_diagnostics .. " diagnostics")
 
-                print("After filtering: " .. #filtered_diagnostics .. " diagnostics")
+								-- Replace with filtered diagnostics
+								result.diagnostics = filtered_diagnostics
 
-                -- Replace with filtered diagnostics
-                result.diagnostics = filtered_diagnostics
+								-- Call the default handler
+								vim.lsp.handlers["textDocument/publishDiagnostics"](_, result, ctx, config)
+							end,
+						},
+					})
+				end,
 
-                -- Call the default handler
-                vim.lsp.handlers["textDocument/publishDiagnostics"](_, result, ctx, config)
-              end
-            }
-          })
-        end,
+				["lua_ls"] = function()
+					require("lspconfig")["lua_ls"].setup({
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
 
-        ["lua_ls"] = function()
-          require("lspconfig")["lua_ls"].setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" }
-                }
-              }
-            }
-          })
-        end,
-
-        ["eslint"] = function()
-          require('lspconfig').eslint.setup {
-            on_attach = function(client, _)
-              client.server_capabilities.documentFormattingProvider = false
-              client.server_capabilities.documentRangeFormattingProvider = false
-            end,
-            root_dir = require('lspconfig').util.root_pattern('.git', 'package.json'),
-          }
-        end
-      })
-    end
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    config = function()
-      local plugin = require("mason-tool-installer")
-      plugin.setup({
-        ensure_installed = {
-          "prettier"
-        }
-      })
-    end
-  },
-  {
-    'stevearc/conform.nvim',
-    config = function()
-      local plugin = require("conform")
-      plugin.setup({
-        formatters_by_ft = {
-          typescript = { "prettier" },
-          javascript = { "prettier" },
-        },
-      })
-    end
-  },
-  {
-    -- Required as it will be called by mason-lspconfig
-    "neovim/nvim-lspconfig",
-    dependencies = { "mason-lspconfig.nvim" },
-    config = function()
-      map('n', '<leader>ld', ':lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true, desc = "LSP: Go to definition" })
-    end
-  }
+				["eslint"] = function()
+					require("lspconfig").eslint.setup({
+						root_dir = require("lspconfig").util.root_pattern(".git", "package.json"),
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			local plugin = require("mason-tool-installer")
+			plugin.setup({
+				ensure_installed = {
+					"prettier",
+					"stylua",
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			local plugin = require("conform")
+			plugin.setup({
+				formatters_by_ft = {
+					typescript = { "prettier" },
+					javascript = { "prettier" },
+					lua = { "stylua" },
+				},
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = { "*.ts", "*.js", "*.tsx", "*.jsx" },
+				callback = function(args)
+					vim.cmd("EslintFixAll")
+					plugin.format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+	{
+		-- Required as it will be called by mason-lspconfig
+		"neovim/nvim-lspconfig",
+		dependencies = { "mason-lspconfig.nvim" },
+		config = function()
+			map(
+				"n",
+				"<leader>ld",
+				":lua vim.lsp.buf.definition()<CR>",
+				{ noremap = true, silent = true, desc = "LSP: Go to definition" }
+			)
+		end,
+	},
 }
-)
