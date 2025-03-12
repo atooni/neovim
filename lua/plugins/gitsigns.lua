bc1e4d1072ca0a22dfdd1526d24095a197dd96fb
Andreas Gies <andreas@wayofquality.de>
2025-03-12 10:51:48 +0100

    Keybindings to navigate hunks

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,28 @@
-return (
-{
+local map = vim.keymap.set
+
+return {
   'lewis6991/gitsigns.nvim',
   config = function()
-    local plugin = require('gitsigns')
+    local plugin = require 'gitsigns'
     plugin.setup()
-  end
+    map('n', '<leader>hn', ':Gitsigns next_hunk<CR>', { noremap = true, silent = true, desc = 'Go to the next hunk' })
+    map(
+      'n',
+      '<leader>hp',
+      ':Gitsigns prev_hunk<CR>',
+      { noremap = true, silent = true, desc = 'Go to the previous hunk' }
+    )
+    map(
+      'n',
+      '<leader>hs',
+      ':Gitsigns stage_hunk<CR>',
+      { noremap = true, silent = true, desc = 'Stage the current hunk' }
+    )
+    map(
+      'n',
+      '<leader>hs',
+      ':Gitsigns unstage_hunk<CR>',
+      { noremap = true, silent = true, desc = 'Unstage the current hunk' }
+    )
+  end,
 }
-)
