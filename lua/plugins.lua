7051ff89d10aed356bf8519c4c054a722c2f749e
Andreas Gies <andreas@wayofquality.de>
2025-03-12 11:12:23 +0100

    Add Diffview plugin config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,14 @@
--- Configure the Lazy Package Manager 
+-- Configure the Lazy Package Manager
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -17,26 +17,27 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  require("plugins.themes"),
-  require("plugins.lsp"),
-  require("plugins.completion"),
-  require("plugins.tree"),
-  require("plugins.fzf-lua"),
-  require("plugins.lazygit"),
-  require("plugins.gitsigns"),
-  require("plugins.lualine"),
+require('lazy').setup {
+  require 'plugins.themes',
+  { 'nvim-tree/nvim-web-devicons', opts = {} },
+  require 'plugins.lsp',
+  require 'plugins.completion',
+  require 'plugins.tree',
+  require 'plugins.fzf-lua',
+  require 'plugins.lazygit',
+  require 'plugins.gitsigns',
+  require 'plugins.lualine',
+  require 'plugins.diffview',
   {
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-treesitter/nvim-treesitter',
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query","typescript" },
+      require('nvim-treesitter.configs').setup {
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'typescript' },
         auto_install = false,
         highlight = {
-          enable = true
-        }
-      })
-    end
-  }
-})
-
+          enable = true,
+        },
+      }
+    end,
+  },
+}
